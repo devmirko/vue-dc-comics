127c6963f805f4c44ed96f8cc603578d5bbfd5ce
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-06-08 14:59:58 +0200

    inizio progetto!!!

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,14 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
+
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+<header>
+  <div class="container">
+      <!-- logo -->
+      <a href="">
+          <img src="../assets/dc-logo.png" alt="questa è una immagine">
+      </a>
+      <!-- contenitore menu -->
+      <ul>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+          <li>
+              <a href="">characters</a>
+          </li>
+
+      </ul>
+      
+  </div>
+</header>  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+   
+
+</style>
